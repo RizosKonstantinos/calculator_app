23384f2a277dab44977e3bf1ed0d4b886c1ee0d4
Konstantinos Rizos <konriz.sd@gmail.com>
2021-10-08 13:35:00 +0300

    Remove unnecessary Action Listener and Action Performed method for a button.

--- src/main/java/com/example/calculator_app/MainWindow.java
+++ src/main/java/com/example/calculator_app/MainWindow.java
@@ -324,10 +324,6 @@ public class MainWindow extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
-
     /**
      * @param args the command line arguments
      */
